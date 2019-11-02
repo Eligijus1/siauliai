656e69ee0a84e1964a8046ae9c4692fd4b75cce4
Eligijus Stugys <eligijus.stugys@gmail.com>
2019-11-02 18:57:28 +0200

    Make menu.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-
-
 let uri = window.location.search.substring(1);
 let params = new URLSearchParams(uri);
 
 // DEBUG:
-console.info("id=",params.get("id"));
+console.info("id=", params.get("id"));
+
+new Vue({
+    el: '#appDebug',
+    data: {
+        json: null
+    },
+    created: function () {
+        fetch("data/index.json")
+            .then(r => r.json())
+            .then(json => {
+                this.json = json;
+            });
+    }
+});
