24e3843f5f747815633e366638244f4d1e8a5b13
Eligijus Stugys <eligijus.stugys@gmail.com>
2019-11-02 19:46:06 +0200

    Make menu.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,44 @@ let params = new URLSearchParams(uri);
 
 // DEBUG:
 console.info("id=", params.get("id"));
+console.info("jsonFile=", params.get("jsonFile"));
+
+
+// var AdminLTEOptions = {
+//     //Enable sidebar expand on hover effect for sidebar mini
+//     //This option is forced to true if both the fixed layout and sidebar mini
+//     //are used together
+//     sidebarExpandOnHover: true,
+//     //BoxRefresh Plugin
+//     enableBoxRefresh: true,
+//     //Bootstrap.js tooltip
+//     enableBSToppltip: true
+// };
+
 
 new Vue({
-    el: '#appDebug',
+    el: '#menuCityCouncilProjects',
     data: {
-        json: null
-    },
-    created: function () {
-        fetch("data/index.json")
-            .then(r => r.json())
-            .then(json => {
-                this.json = json;
-            });
+        json: {CityCouncilProjects: [
+            { "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json", name: 'Test 1' },
+            { "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json", name: 'Test 2' },
+            { "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json", name: 'Test 3' }
+        ]}
     }
 });
+
+
+// new Vue({
+//     el: '#appDebug',
+//     data: {
+//         json: null
+//     },
+//     created: function () {
+//         fetch("data/index.json")
+//             .then(r => r.json())
+//             .then(json => {
+//                 this.json = json;
+//             });
+//     }
+// });
+
