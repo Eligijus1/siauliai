44aa2c70d26009dae326b9713211ce2890b43b47
Eligijus Stugys <eligijus.stugys@real-digital.de>
2019-11-06 09:35:44 +0200

    Fixing menu.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,70 @@
+// Define global variables:
 let uri = window.location.search.substring(1);
 let params = new URLSearchParams(uri);
 
 // DEBUG:
-console.info("id=", params.get("id"));
-console.info("jsonFile=", params.get("jsonFile"));
-
-
-// var AdminLTEOptions = {
-//     //Enable sidebar expand on hover effect for sidebar mini
-//     //This option is forced to true if both the fixed layout and sidebar mini
-//     //are used together
-//     sidebarExpandOnHover: true,
-//     //BoxRefresh Plugin
-//     enableBoxRefresh: true,
-//     //Bootstrap.js tooltip
-//     enableBSToppltip: true
-// };
-
+//console.info("id=", params.get("id"));
+//console.info("jsonFile=", params.get("jsonFile"));
 
 new Vue({
     el: '#menuCityCouncilProjects',
     data: {
-        json: {CityCouncilProjects: [
-            { "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json", name: 'Test 1' },
-            { "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json", name: 'Test 2' },
-            { "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json", name: 'Test 3' }
-        ]}
-    }
-});
-
-
-// new Vue({
-//     el: '#appDebug',
-//     data: {
-//         json: null
-//     },
-//     created: function () {
+        json: {
+            CityCouncilProjects: [
+                {
+                    "id": "menu_1",
+                    "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json",
+                    name: 'Test 1',
+                    "enabled": true
+                },
+                {
+                    "id": "menu_2",
+                    "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json",
+                    name: 'Test 2',
+                    "enabled": true
+                },
+                {
+                    "id": "menu_3",
+                    "file": "SiauiaiCityCouncilProjects_2019-11-07_13_00_00.json",
+                    name: 'Test 3',
+                    "enabled": true
+                }
+            ]
+        },
+        activeClass: 'active',
+        navLinkClass: 'nav-link',
+        activeItem: null
+    },
+    created: function () {
+        console.info("created");
 //         fetch("data/index.json")
 //             .then(r => r.json())
 //             .then(json => {
 //                 this.json = json;
 //             });
-//     }
-// });
-
+    },
+    methods: {
+        isActive: function (menuItem) {
+            return this.activeItem === menuItem
+        },
+        setActive(menuItem, event) {
+            // vm = this;
+            // Vue.set(this.$data, 'isActive', true)
+            //let element = this.$ref[projectId];
+            //console.info("projectId=",projectId);
+            //console.info("event=",event);
+            //console.info("event.target=",event.target);
+            //console.info("this.id=",this.id);
+            //console.info("this=",this);
+            //console.info("member=",member);
+            //console.info("element=",element);
+            //console.info("this.$refs=",this.$refs[projectId]);
+            //className: "nav-link"
+            //<a href="../UI/modals.html" class="nav-link active">
+            //console.info("this.$refs=", this.$refs[projectId]);
+            //console.info("this.$refs=", this.$refs[projectId][0]);
+            //this.$refs[projectId][0].addClass("active");
+            this.activeItem = menuItem;
+        }
+    },
+});
